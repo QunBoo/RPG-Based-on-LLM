8945fe1f1d7981a82728e37fbe2ce685c9b63bb4
Qunboo <qunboo@example.com>
2023-12-07 22:21:08 +0800

    实现webSocket连接，可以通过postman进行ws连接了2

--- server/serverimpl/WebSocket/WebSocket.go
+++ server/serverimpl/WebSocket/WebSocket.go
@@ -56,30 +56,3 @@ func StartWebSocket(config *config.Config, clientManager *ClientManager, logger 
 	http.ListenAndServe(":"+webSocketPort, nil)
 
 }
-
-//// 升级协议，在tpl文件中访问对应的链路实现函数调用
-//func wsPage(w http.ResponseWriter, req *http.Request) {
-//
-//	// 升级协议
-//	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
-//		fmt.Println("升级协议", "ua:", r.Header["User-Agent"], "referer:", r.Header["Referer"])
-//
-//		return true
-//	}}).Upgrade(w, req, nil)
-//	if err != nil {
-//		http.NotFound(w, req)
-//
-//		return
-//	}
-//
-//	fmt.Println("webSocket 建立连接:", conn.RemoteAddr().String())
-//
-//	currentTime := uint64(time.Now().Unix())
-//	client := NewClient(conn.RemoteAddr().String(), conn, currentTime)
-//
-//	go client.read()
-//	go client.write()
-//
-//	//TODO 用户连接事件
-//	//clientManager.Register <- client
-//}
